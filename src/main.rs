eb99e2ff845895231be3de0367abe5d85a44d259
Ægir <0x5ea000000@gmail.com>
2024-08-02 20:12:21 +0700

    feat: add more template
    Signed-off-by: Ægir <0x5ea000000@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -37,27 +37,28 @@ pub fn do_nothing_filter(value: &Value, _: &HashMap<String, Value>) -> Result<Va
 }
 
 pub enum Op {
-    Expmod
+    Expmod,
+    ExpmodSpecial,
+    ExpmodTraceGen,
 }
 struct Instruction {
     op: Op,
     data: Vec<U256>,
 }
-struct Memory {
+#[derive(Copy, Clone, Debug)]
+struct MemoryRange {
     start: U256,
     end: U256,
-    description: String,
 }
 
-
 struct SolidityData {
-    memory_layout: HashMap<String, String>,
+    memory_layout: HashMap<String, MemoryRange>,
     memory: HashMap<String, U256>,
     instructions: Vec<Instruction>,
 }
 
 enum ParserOutput {
-    Memory(Memory),
+    Memory(String, MemoryRange),
     Instruction(Instruction),
     None,
 }
@@ -71,8 +72,8 @@ fn reader(sol_path: &str) -> Vec<String> {
     let single_line_re = Regex::new(r"//.*").unwrap();
     let mut comments = Vec::new();
     // Extract single-line comments
-    for single_comment in single_line_re.find_iter(&sol_code) {
-        comments.push(single_comment.as_str().to_string());
+    for mut single_comment in single_line_re.find_iter(&sol_code) {
+        comments.push(single_comment.as_str().trim_end_matches('.').to_string());
     }
     comments
 }
@@ -85,10 +86,9 @@ fn parser(comment: String) -> ParserOutput {
              let start_value = &captures[1];
              let end_value = &captures[2];
              let description = &captures[3].trim();
-             ParserOutput::Memory(Memory {
+             ParserOutput::Memory(description.to_string(), MemoryRange {
                  start: U256::from_str_radix(start_value, 16).unwrap(),
                  end: U256::from_str_radix(end_value, 16).unwrap(),
-                 description: description.to_string(),
              })
              // format!("// MOVE: start: {}, end: {}, description: {}", start_value, end_value, description)
          })),
@@ -104,7 +104,26 @@ fn parser(comment: String) -> ParserOutput {
              ParserOutput::Instruction(Instruction { op: Op::Expmod, data })
              // format!("// MOVE: expmods index: {}, divisor: {}", first_value, second_value)
          })),
-        //expmods[9] = trace_generator^(trace_length / 64).
+        // expmods[8] = point^trace_length.
+        (Regex::new(r"//\s*expmods\[(\d+)\]\s*=\s*point\^trace_length").unwrap(),
+         Box::new(|captures: &regex::Captures| {
+             let first_value = &captures[1];
+             let data = vec![
+                 U256::from_str_radix(first_value, 10).unwrap(),
+             ];
+             ParserOutput::Instruction(Instruction { op: Op::ExpmodSpecial, data })
+         })),
+        // expmods[9] = trace_generator^(trace_length / 64).
+        (Regex::new(r"//\s*expmods\[(\d+)\]\s*=\s*trace_generator\^\(trace_length\s*/\s*(\d+)\)").unwrap(),
+         Box::new(|captures: &regex::Captures| {
+             let first_value = &captures[1];
+             let second_value = &captures[2];
+             let data = vec![
+                 U256::from_str_radix(first_value, 10).unwrap(),
+                 U256::from_str_radix(second_value, 10).unwrap(),
+             ];
+             ParserOutput::Instruction(Instruction { op: Op::ExpmodTraceGen, data })
+         })),
     ];
 
     for (regex, processor) in patterns {
@@ -132,47 +151,103 @@ fn extractor(parser_output: ParserOutput) {
     // }
 }
 
-// fn calculate_memory() -> {
-//
-// }
 fn main() {
     let comments = reader("examples/cpu-constraint-poly/CpuConstraintPoly.sol");
+    let mut context = Context::new();
+
+    let mut solidity_data = SolidityData {
+        memory_layout: HashMap::new(),
+        memory: HashMap::new(),
+        instructions: Vec::new(),
+    };
 
     let mut memory_layout = Vec::new();
     let mut expmods_data = Vec::new();
+    let mut expmods_special_data = Vec::new();
+    let mut expmods_trace_gen_data = Vec::new();
 
     for comment in comments {
         let parsed = parser(comment);
         match parsed {
-            ParserOutput::Memory(mem) => {
+            ParserOutput::Memory(des, mem) => {
+                solidity_data.memory_layout.insert(des.clone(), mem.clone());
                 let mut map: HashMap<&str, String> = HashMap::new();
-                let Memory { start, end, description } = mem;
+                let MemoryRange { start, end } = mem;
                 map.insert("start", format!("{:#x}", start));
                 map.insert("end", format!("{:#x}", end));
-                map.insert("description", description);
+                map.insert("description", des);
                 memory_layout.push(map);
             }
             ParserOutput::Instruction(ins) => {
-                match ins.op {
-                    Op::Expmod => {
-                        let mut map: HashMap<&str, String> = HashMap::new();
-                        let Instruction { op, data, } = ins;
-                        let divisor = data[1];
-                        println!("memory_address: 0x23c0, divisor: {}", divisor);
-                        map.insert("memory_address", "0x23c0".parse().unwrap());
-                        map.insert("divisor", divisor.to_string());
-                        expmods_data.push(map);
-                    }
-                }
+                solidity_data.instructions.push(ins);
             }
             ParserOutput::None => {}
         }
     }
 
-    let mut context = Context::new();
+    for mem in solidity_data.memory_layout.iter() {
+        context.insert(mem.0, &mem.1.start);
+    }
+    let mut instruction_code = String::new();
+
+    for ins in solidity_data.instructions {
+        let mut template_name = "";
+        match ins.op {
+            Op::Expmod => {
+                let mut map: HashMap<&str, String> = HashMap::new();
+                let Instruction { op, data, } = ins;
+                let divisor = data[1];
+                // calculate the memory address
+                let start = solidity_data.memory_layout.get("expmods").unwrap().start;
+                let address = start + U256::from(32) * data[0];
+                context.insert("memory_address", &format!("{:#x}", address));
+                context.insert("divisor", &divisor.to_string());
+                // map.insert("memory_address", format!("{:#x}", address));
+                // map.insert("divisor", divisor.to_string());
+                expmods_data.push(map);
+                template_name = "sol/expmod.sol.template";
+            }
+            Op::ExpmodSpecial => {
+                let mut map: HashMap<&str, String> = HashMap::new();
+                let Instruction { op, data, } = ins;
+                // calculate the memory address
+                let start = solidity_data.memory_layout.get("expmods").unwrap().start;
+                let address = start + U256::from(32) * data[0];
+                context.insert("memory_address", &format!("{:#x}", address));
+                // map.insert("memory_address", format!("{:#x}", address));
+                expmods_special_data.push(map);
+                template_name = "sol/expmod-special.sol.template";
+
+            }
+            Op::ExpmodTraceGen => {
+                let mut map: HashMap<&str, String> = HashMap::new();
+                let Instruction { op, data, } = ins;
+                let divisor = data[1];
+                // calculate the memory address
+                let start = solidity_data.memory_layout.get("expmods").unwrap().start;
+                let address = start + U256::from(32) * data[0];
+                context.insert("memory_address", &format!("{:#x}", address));
+                context.insert("divisor", &divisor.to_string());
+                // map.insert("memory_address", format!("{:#x}", address));
+                // map.insert("divisor", divisor.to_string());
+                expmods_trace_gen_data.push(map);
+                template_name = "sol/expmod-trace-gen.sol.template";
+
+            }
+        }
+
+        instruction_code.push_str(&TEMPLATES.render(template_name, &context).unwrap());
+        instruction_code.push_str("\n");
+    }
+
+    // for data in &expmods_special_data {
+    //     println!("{:?}", data);
+    // }
+
+    context.insert("memory_layout", &memory_layout);
 
     context.insert("expmods", &expmods_data);
-    context.insert("memory_layout", &memory_layout);
+    context.insert("expmods_special", &expmods_special_data);
 
 
     context.insert("username", &"Bob");
@@ -180,8 +255,7 @@ fn main() {
     context.insert("show_all", &false);
     context.insert("bio", &"<script>alert('pwnd');</script>");
 
-    // A one off template
-    Tera::one_off("hello", &Context::new(), true).unwrap();
+    context.insert("instructions", &instruction_code);
 
     match TEMPLATES.render("sol/a.sol.template", &context) {
         Ok(s) => {
